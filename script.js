baa6535d5161edb9c45411a61e6a7319c500f0af
Debarjit mohanty <91021174+Debarjitmohanty@users.noreply.github.com>
2023-12-01 10:45:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
